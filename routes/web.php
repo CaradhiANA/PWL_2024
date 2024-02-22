8215eeebac54ca7cf7b6c7dc6534a3e91e866936
caradhiANA <aleganaufal@example.com>
2024-02-22 15:54:54 +0700

    Jobsheet 2 bag 1 controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Alega Naufal Akbar (2141762058)';
+});
+
+
+Route::get('/posts/{post}/comments/{comment}', function 
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+    });
+
+Route::get('/articles/{id}', function ($id) {
+    return 'ID '.$id;
+    });
+
+Route::get('/user/{name?}', function ($name='Jonh') {   
+    return 'Nama saya '.$name;
+});
+
+
+ 
+
+
+    
